6d53243668167f89ef932de01d928e4891bfd668
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-22 20:31:38 +0800

    demo2 @GetMapping @PostMapping两种注解是限定了请求方式

--- springMVC-demo2/src/main/java/com/litway/controller/RequestMappingController.java
+++ springMVC-demo2/src/main/java/com/litway/controller/RequestMappingController.java
@@ -2,6 +2,8 @@ package com.litway.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,5 +21,25 @@ public class RequestMappingController {
         return "success";
     }
 
+    // @GetMapping是@RequestMapping注即的派生注解, 表示的是, 仅支持GET请求方法. 这样简化了@RequestMapping注解
+    @GetMapping("/testGetMapping")
+    public String getMapping() {
+        System.out.println("getMapping");
+        return "success";
+    }
+
+
+    @PostMapping("/testPostMapping")
+    public String testPostMapping() {
+        System.out.println("testPostMapping");
+        return "success";
+    }
+
+
+    @RequestMapping(value = "/testPut", method = RequestMethod.PUT)
+    public String testPut() {
+        System.out.println("testPut");
+        return "success";
+    }
 
 }
