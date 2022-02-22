5677c1c337d901c8f5ef5cc83976bef2dcc75d99
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-22 18:21:04 +0800

    demo2 method参数用来表示支持的方法 除此之外的不支持 前提value值也必须匹配上才可以

--- springMVC-demo2/src/main/java/com/litway/controller/RequestMappingController.java
+++ springMVC-demo2/src/main/java/com/litway/controller/RequestMappingController.java
@@ -3,6 +3,7 @@ package com.litway.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/hello")
@@ -10,7 +11,9 @@ public class RequestMappingController {
 
     @RequestMapping(
             // 只要客户端的请求能够满足其中任何一个路径就能够映射到该方法来.
-            value = {"/testRequestMapping", "/test"}
+            value = {"/testRequestMapping", "/test"},
+            // 现在仅支持GET 和 POST请求方式. 前提是value地址必须匹配一致
+            method = {RequestMethod.GET, RequestMethod.POST}
     )
     public String success() {
         return "success";
