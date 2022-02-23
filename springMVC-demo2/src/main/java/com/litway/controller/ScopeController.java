d469672644fe29dd0fff576893854f2248e4bba8
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-23 15:17:59 +0800

    demo2 集中返回数据的方法: HttpServletAPI Model Map ModelMap 这四种都必须作为想用方法形参. ModelAndView需要作为响应方法中的返回值.

--- springMVC-demo2/src/main/java/com/litway/controller/ScopeController.java
+++ springMVC-demo2/src/main/java/com/litway/controller/ScopeController.java
@@ -2,6 +2,7 @@ package com.litway.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -9,10 +10,19 @@ import java.util.Map;
 @Controller
 public class ScopeController {
 
+    // 通过向形参Map中添加数据, 在前端输出request域. 发现输出的就是添加的数据. 因此, 此处的map本质与request域有关.
     @RequestMapping("/testMap")
     public String testMap(Map<String, Object> map) {
         // 形参是Map集合. 向map中添加数据就是request域中添加数据.
         map.put("testRequestScope", "hello, Map");
         return "success";
     }
+
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap mm) {
+        mm.addAttribute("testRequestScope", "hello, ModelMap");
+        return "success";
+    }
+
+
 }
