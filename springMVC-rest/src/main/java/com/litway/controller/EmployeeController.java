5d43cb86e64ec165254f14210426aa9871cca49b
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-24 15:49:57 +0800

    rest 添加用户的实现

--- springMVC-rest/src/main/java/com/litway/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/litway/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import com.litway.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -43,9 +40,9 @@ public class EmployeeController {
     }
 
 
-
-
-
-
-
+    @PostMapping
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
 }
