e8769dc0273f9e27b54f7a0c91880ce8b5327352
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-25 18:51:51 +0800

    demo3 测试控制器

---
+++ springMVC-demo3/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Date:2021/7/7
+ * Author:ybc
+ * Description:
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/test_view")
+    public String testView() {
+        System.out.println("testView");
+        return "test_view";
+    }
+}
